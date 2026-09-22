0b06d551e70b44c9632bb8e6438e3eed0d627fa5
Hwang <yugecosecant@foxmail.com>
2026-09-22 21:06:56 +0800

    #完善设置页面

--- lib/scene/settings_scene.dart
+++ lib/scene/settings_scene.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart' hide OverlayRoute;
 import 'package:tank90/app/provider/global_config.dart';
 import 'package:tank90/app/tank_war_game.dart';
 import 'package:tank90/data/game_constants.dart';
+import 'package:tank90/data/game_properties.dart';
 
 /// 设置弹窗界面
 class SettingsScene extends PositionComponent
@@ -14,16 +15,27 @@ class SettingsScene extends PositionComponent
   late final Sprite _musicOpenSprite;
   late final Sprite _musicCloseSprite;
   late final SpriteComponent? _musicStatusSprite;
+  late final List<Sprite> _gameLevelSprites;
+  late final SpriteComponent _gameLevelSprite;
 
   @override
   FutureOr<void> onLoad() async {
     await super.onLoad();
     _musicCloseSprite = await Sprite.load('music_close.png');
     _musicOpenSprite = await Sprite.load('music_open.png');
+    _gameLevelSprites = [
+      await Sprite.load('game_easy.png'),
+      await Sprite.load('game_normal.png'),
+      await Sprite.load('game_difficult.png'),
+    ];
     var offsetX = (GameConstants.CANVAS_SIZE.x - 300.0) / 2.0;
     var offsetY = (GameConstants.CANVAS_SIZE.y - 200.0) / 2.0;
     var offsetMx = GameConstants.CANVAS_SIZE.x - offsetX;
     var offsetMy = GameConstants.CANVAS_SIZE.y - offsetY;
+    var circlePaint = Paint()
+      ..isAntiAlias = true
+      ..style = .fill
+      ..color = Colors.transparent;
     add(
       SpriteComponent(
         sprite: await Sprite.load('game_settings.png'),
@@ -44,6 +56,41 @@ class SettingsScene extends PositionComponent
       ),
     );
     add(
+      SpriteComponent(
+        sprite: await Sprite.load('game_level.png'),
+        position: Vector2(offsetX + 16, offsetY + 94),
+      ),
+    );
+    add(
+      PositionComponent(
+        position: Vector2(offsetMx - 148, offsetY + 94),
+        children: [
+          _gameLevelSprite = SpriteComponent(sprite: _gameLevelSprites.first),
+          ButtonComponent(
+            anchor: .center,
+            size: Vector2.all(12),
+            position: Vector2(35, 16),
+            onPressed: () => _toggleGameLevel(.easy),
+            button: CircleComponent(radius: 6, paint: circlePaint),
+          ),
+          ButtonComponent(
+            anchor: .center,
+            size: Vector2.all(12),
+            position: Vector2(80, 16),
+            onPressed: () => _toggleGameLevel(.normal),
+            button: CircleComponent(radius: 6, paint: circlePaint),
+          ),
+          ButtonComponent(
+            anchor: .center,
+            size: Vector2.all(12),
+            position: Vector2(121, 16),
+            onPressed: () => _toggleGameLevel(.difficulty),
+            button: CircleComponent(radius: 6, paint: circlePaint),
+          ),
+        ],
+      ),
+    );
+    add(
       ButtonComponent(
         anchor: .center,
         onPressed: _closeDialog,
@@ -59,13 +106,11 @@ class SettingsScene extends PositionComponent
   void onMount() {
     addToGameWidgetBuild(() {
       var globalConfig = ref.read(globalConfigProvider);
-      _musicStatusSprite?.sprite = globalConfig.soundAvailable
-          ? _musicCloseSprite
-          : _musicOpenSprite;
+      _setGameLevelSprite(globalConfig.gameLevel);
+      _setGameMusicStatusSprite(globalConfig.soundAvailable);
       ref.listen(globalConfigProvider, (_, next) {
-        _musicStatusSprite?.sprite = next.soundAvailable
-            ? _musicCloseSprite
-            : _musicOpenSprite;
+        _setGameLevelSprite(next.gameLevel);
+        _setGameMusicStatusSprite(next.soundAvailable);
       });
     });
     super.onMount();
@@ -77,14 +122,31 @@ class SettingsScene extends PositionComponent
     super.render(canvas);
   }
 
+  /// 设置音乐开关的sprite
+  void _setGameMusicStatusSprite(bool isAvailable) {
+    _musicStatusSprite?.sprite = isAvailable
+        ? _musicCloseSprite
+        : _musicOpenSprite;
+  }
+
   /// 切换游戏声音状态
   void _toggleGameMusicStatus() {
     var globalConfig = ref.read(globalConfigProvider);
     var targetValue = !globalConfig.soundAvailable;
-    _musicStatusSprite?.sprite = targetValue
-        ? _musicCloseSprite
-        : _musicOpenSprite;
     ref.read(globalConfigProvider.notifier).soundAvailable = targetValue;
+  }
+
+  /// 设置游戏等级的sprite
+  void _setGameLevelSprite(GameLevel target) {
+    _gameLevelSprite.sprite = (target == .easy
+        ? _gameLevelSprites.first
+        : (target == .normal ? _gameLevelSprites[1] : _gameLevelSprites.last));
+  }
+
+  /// 切换游戏等级设置
+  void _toggleGameLevel(GameLevel target) {
+    debugPrint('current target: $target');
+    ref.read(globalConfigProvider.notifier).gameLevel = target;
   }
 
   /// 关闭当前页面
@@ -98,7 +160,7 @@ extension _CanvasDrawer on Canvas {
     var paint = Paint()
       ..isAntiAlias = true
       ..style = .fill
-      ..color = Colors.white.withValues(alpha: 0.8);
+      ..color = const Color.fromARGB(255, 128, 128, 128).withValues(alpha: 0.8);
     drawRoundRectBackground(paint);
     paint
       ..style = .stroke
